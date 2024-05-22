2f87cb30a01d450ff95b3a2aa654e91d334eb813
Erxhan Ganiu <ganijerd@students.zhaw.ch>
2024-05-22 22:04:35 +0200

    Add test for valid password length

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/PasswordValidator.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/PasswordValidator.java
@@ -1,5 +1,7 @@
 package ch.zhaw.iwi.devops.demo;
 
 public class PasswordValidator {
-    
+    public static boolean isValidLength(String password) {
+        return password.length() >= 8;
+    }
 }
