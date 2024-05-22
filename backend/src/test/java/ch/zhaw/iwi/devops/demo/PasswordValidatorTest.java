406a4ea1245a8c639934b3fbf81536a28be0d31b
Erxhan Ganiu <ganijerd@students.zhaw.ch>
2024-05-23 01:23:27 +0200

    update imports

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/PasswordValidatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/PasswordValidatorTest.java
@@ -1,10 +1,7 @@
 package ch.zhaw.iwi.devops.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
