29cc3365543a80119af33837e4e630bc2482baa4
Erxhan Ganiu <ganijerd@students.zhaw.ch>
2024-05-22 21:58:45 +0200

    Add test for valid password length

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/PasswordValidatorTest.java
@@ -0,0 +1,15 @@
+package ch.zhaw.iwi.devops.demo;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PasswordValidatorTest {
+    @Test
+    void testIsValidLength() {
+        assertTrue(PasswordValidator.isValidLength("12345678"));
+        assertFalse(PasswordValidator.isValidLength("123"));
+        assertTrue(PasswordValidator.isValidLength("1234567890123456"));
+    }
+}
